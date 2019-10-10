ef828955a1559f175409431deb11df4f2ddd2ccd
vladislav.ovcharenk <vladislav.ovcharenk@ihsmarkit.com>
2019-10-10 10:06:20 +0200

    cherrypy + webhooks

--- cards_bot.py
+++ cards_bot.py
@@ -1,13 +1,41 @@
-from telebot import types
-import config
-import telebot
+import sys
 
+import cherrypy
+import telebot
+from telebot import types
+
+import config
 from cards_dao import CardsDao
 
 bot = telebot.TeleBot(config.token)
-dao = CardsDao('cards.db')
+dao = CardsDao(sys.argv[1])
 
 last_id = None
+
+WEBHOOK_HOST = '63.34.10.164'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % config.token
+
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                'content-type' in cherrypy.request.headers and \
+                cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
 
 
 @bot.message_handler(commands=['random'])
@@ -36,4 +64,22 @@ def echo(message):
     bot.send_message(message.chat.id, 'Send /random to get random question.', reply_markup=markup)
 
 
-bot.polling()
+# bot.polling()
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
